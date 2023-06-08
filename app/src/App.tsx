67f1f2883b299e91ca8d3d873e2552a3ee9af4a5
andyhsu <andy57291@gmail.com>
2023-06-08 15:31:29 +0000

    Add lazy loading to app root file

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,21 +1,25 @@
 import './assets/css/App.css';
-import Home from './pages/Home';
-import GetStarted from './pages/GetStarted';
-import Profile from './pages/Profile';
-import Register from './pages/Register';
-import SelectMovies from './pages/SelectMovies';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
+
+const Home = lazy(() => import('./pages/Home'));
+const GetStarted = lazy(() => import('./pages/GetStarted'));
+const Profile = lazy(() => import('./pages/Profile'));
+const Register = lazy(() => import('./pages/Register'));
+const SelectMovies = lazy(() => import('./pages/SelectMovies'));
 
 function App() {
   return (
     <div className="app">
       <Router>
         <Routes>
-          <Route path="/" element={<GetStarted />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/selectMovies" element={<SelectMovies />} />
+          <Suspense fallback={<h1>Loading...</h1>}>
+            <Route path="/" element={<GetStarted />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/selectMovies" element={<SelectMovies />} />
+          </Suspense>
         </Routes>
       </Router>
     </div>
