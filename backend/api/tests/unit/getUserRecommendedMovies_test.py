15a4d554b1c4bb10a23d9a50920329e86e5f6af2
andy145155 <andy57291@gmail.com>
2024-06-16 03:52:31 +0800

    Add unit test for api

--- backend/api/tests/unit/getUserRecommendedMovies_test.py
+++ backend/api/tests/unit/getUserRecommendedMovies_test.py
@@ -1,7 +1,8 @@
 import os
 import json
 from unittest import TestCase
-from unittest.mock import MagicMock, patch
+from unittest.mock import MagicMock, patch, Mock
+from utils.types import UserSelection
 from moto import mock_aws
 from utils.classes import LambdaDynamoDBClass
 from boto3 import resource, client
@@ -98,7 +99,7 @@ class TestGetUserRecommenedMoviesLambda(TestCase):
             }
         )
 
-    def test_fetch_user_selection(self):
+    def test_fetch_user_selection(self) -> None:
         """
         Test fetch_user_selection function
         """
@@ -111,7 +112,9 @@ class TestGetUserRecommenedMoviesLambda(TestCase):
             "selectedMovies": [1],
         }
 
-        result = fetch_user_selection(email, self.mocked_user_selection_dynamodb_class)
+        result: UserSelection = fetch_user_selection(
+            email, self.mocked_user_selection_dynamodb_class
+        )
         self.assertEqual(result, expected_return_value)
 
         empty_email = "empty@gmail.com"
@@ -121,10 +124,88 @@ class TestGetUserRecommenedMoviesLambda(TestCase):
             "selectedMovies": [],
         }
 
-        result = fetch_user_selection(
+        result: UserSelection = fetch_user_selection(
             empty_email, self.mocked_user_selection_dynamodb_class
         )
         self.assertEqual(result, expected_return_value)
+
+    def test_validate_input(self) -> None:
+        """
+        Test validate_input function
+        """
+        from functions.getUserRecommendMovies import validate_input
+
+        input = {
+            "email": "test@gmail.com",
+            "recommendedMovies": [1, 2],
+            "selectedMovies": [1],
+        }
+
+        expected_return_value = ([1, 2], [1])
+        result = validate_input(input)
+        self.assertEqual(expected_return_value, result)
+
+        input = {
+            "email": "",
+            "recommendedMovies": [],
+            "selectedMovies": [],
+        }
+
+        expected_return_value = ([], [])
+        result = validate_input(input)
+        self.assertEqual(expected_return_value, result)
+
+    def load_sample_event_from_file(self, test_event_file_name: str) -> dict:
+        """
+        Loads and validate test events from the file system
+        """
+        event_file_name = f"tests/events/{test_event_file_name}.json"
+        with open(event_file_name, "r", encoding="UTF-8") as file_handle:
+            event = json.load(file_handle)
+            return event
+
+    @patch("utils.classes.LambdaDynamoDBClass")
+    @patch("utils.functions.ok")
+    @patch("functions.getUserRecommendMovies.fetch_user_selection")
+    @patch("utils.functions.get_movies_by_id_list")
+    def test_lambda_handler_valid_event(
+        self,
+        patch_fetch_movie_by_index: MagicMock,
+        patch_fetch_user_selection: MagicMock,
+        patch_ok: MagicMock,
+        patch_lambda_dynamodb_class: MagicMock,
+    ):
+
+        # patch both movie_dynamodb_resource and user_dynamodb_resource
+        patch_lambda_dynamodb_class.side_effect = [
+            self.mocked_user_selection_dynamodb_class,
+            self.mocked_movies_dynamodb_class,
+        ]
+
+        patch_fetch_user_selection.return_value = {
+            "email": "test@gmail.com",
+            "recommendedMovies": [1, 2],
+            "selectedMovies": [1],
+        }
+
+        patch_fetch_movie_by_index.return_value = {"movieId": 1, "index": 1}
+
+        expected_return_value = {
+            "statusCode": 200,
+            "headers": {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Credentials": "true",
+            },
+            "body": '{"email": "test@gmail.com", "recommendedMovies": [{"movieId": "1", "index": "1"}, {"movieId": "2", "index": "2"}], "selectedMovies": [{"movieId": "1", "index": "1"}]}',
+        }
+
+        from functions.getUserRecommendMovies import handler
+
+        test_event = self.load_sample_event_from_file("getUserRecommendedMovies")
+        actual_return_value = handler(event=test_event, context=None)
+
+        self.assertEqual(actual_return_value, expected_return_value)
 
     def tearDown(self) -> None:
         dynamodb_resource = client("dynamodb", region_name="us-east-1")
