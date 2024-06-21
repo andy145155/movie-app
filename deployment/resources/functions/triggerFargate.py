d744ea80aefd1d2d8ed049eac38ebf768401d3b4
andy145155 <76717347+andy145155@users.noreply.github.com>
2024-06-21 17:27:04 +0000

    Checkin lambda serverless deployment file

---
+++ deployment/resources/functions/triggerFargate.py
@@ -0,0 +1,98 @@
+import boto3
+from botocore.exceptions import ClientError
+import os
+import logging
+import json
+from utils.functions import get_s3_info_from_sns
+from utils.constants import TMDB_5000_CSV, MOVIE_APP_REGION_NAME
+from typing import List
+
+# Get environment variables
+MOVIE_APP_ESC_CLUSTER_ARN = os.getenv("MOVIE_APP_ESC_CLUSTER_ARN")
+DATA_PROCESSING_TASK_DEFINITION_ARN = os.getenv("DATA_PROCESSING_TASK_DEFINITION_ARN")
+MOVIES_SIMILARITY_DYNAMO_DB_NAME = os.getenv("MOVIES_SIMILARITY_DYNAMO_DB_NAME")
+MOVE_CSV_PROCESSED_BUCKET_NAME = os.getenv("MOVE_CSV_PROCESSED_BUCKET_NAME")
+MOVE_CSV_SOURCE_BUCKET_NAME = os.getenv("MOVE_CSV_SOURCE_BUCKET_NAME")
+SUBNET_A_ID = os.getenv("SUBNET_A_ID")
+SUBNET_B_ID = os.getenv("SUBNET_B_ID")
+FARGATE_SG_ID = os.getenv("FARGATE_SG_ID")
+
+# Check if environment variables are set
+if not MOVIE_APP_ESC_CLUSTER_ARN:
+    raise ValueError("Missing environment variable: MOVIE_APP_ESC_CLUSTER_ARN")
+if not DATA_PROCESSING_TASK_DEFINITION_ARN:
+    raise ValueError(
+        "Missing environment variable: DATA_PROCESSING_TASK_DEFINITION_ARN"
+    )
+if not MOVIES_SIMILARITY_DYNAMO_DB_NAME:
+    raise ValueError("Missing environment variable: MOVIES_SIMILARITY_DYNAMO_DB_NAME")
+if not MOVE_CSV_PROCESSED_BUCKET_NAME:
+    raise ValueError("Missing environment variable: MOVE_CSV_PROCESSED_BUCKET_NAME")
+if not MOVE_CSV_SOURCE_BUCKET_NAME:
+    raise ValueError("Missing environment variable: MOVE_CSV_SOURCE_BUCKET_NAME")
+if not SUBNET_A_ID:
+    raise ValueError("Missing environment variable: SUBNET_A_ID")
+if not SUBNET_B_ID:
+    raise ValueError("Missing environment variable: SUBNET_B_ID")
+if not FARGATE_SG_ID:
+    raise ValueError("Missing environment variable: FARGATE_SG_ID")
+
+# Set up logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+# Create aws client
+s3 = boto3.client("s3", region_name=MOVIE_APP_REGION_NAME)
+ecs_client = boto3.client("ecs")
+
+
+def handler(event, context) -> None:
+    logger.info(event)
+
+    bucket, key = get_s3_info_from_sns(event)
+    logger.info(f"Bucket name: {bucket}")
+
+    s3_objects = list_s3_objects(bucket, prefix=TMDB_5000_CSV["PREFIX"])
+    logger.info(f"Bucket objects: {s3_objects}")
+
+    required_files = {TMDB_5000_CSV["CREDITS"], TMDB_5000_CSV["MOVIES"]}
+    if not all(s3_object["Key"] in required_files for s3_object in s3_objects):
+        missing_files = required_files - {s3_object["Key"] for s3_object in s3_objects}
+        return f"The following files are missing: {', '.join(missing_files)}"
+
+    response = runTask(
+        ecs_client,
+        cluster=MOVIE_APP_ESC_CLUSTER_ARN,
+        task_definition=DATA_PROCESSING_TASK_DEFINITION_ARN,
+    )
+    logger.info(response)
+
+
+def list_s3_objects(bucket: str, prefix: str = None) -> List[object]:
+    try:
+        response = s3.list_objects(
+            Bucket=bucket,
+            Prefix=prefix,
+        )
+        return response.get("Contents", [])
+    except ClientError as error:
+        logger.error(f"Error listing objects in bucket {bucket}: {error}")
+        return []
+
+
+def runTask(client, cluster: str, task_definition: str) -> str:
+    response = client.run_task(
+        cluster=cluster,
+        count=1,
+        launchType="FARGATE",
+        networkConfiguration={
+            "awsvpcConfiguration": {
+                "subnets": [SUBNET_A_ID, SUBNET_B_ID],
+                "securityGroups": [FARGATE_SG_ID],
+                "assignPublicIp": "ENABLED",
+            }
+        },
+        taskDefinition=task_definition,
+    )
+
+    return json.dumps(response, default=str)
---
+++ deployment/resources/tests/unit/setUserSelectedMovies_test.py
@@ -0,0 +1,161 @@
+import os
+import json
+from unittest import TestCase
+from unittest.mock import MagicMock, patch, Mock
+from utils.types import UserSelection
+from moto import mock_aws
+from utils.classes import LambdaDynamoDBClass
+from boto3 import resource, client
+
+"""Mocked AWS Credentials for moto."""
+os.environ["AWS_ACCESS_KEY_ID"] = "testing"
+os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
+os.environ["AWS_SECURITY_TOKEN"] = "testing"
+os.environ["AWS_SESSION_TOKEN"] = "testing"
+os.environ["AWS_DEFAULT_REGION"] = "us-east-1"
+
+
+# Mock the DynamoDB Service during the test
+@mock_aws
+class TestSetUserSelectedMovies(TestCase):
+    """
+    Test class for the application setUserSelectedMovies Lambda function
+    """
+
+    def setUp(self) -> None:
+
+        self.movie_ddb_table_name = "movie_ddb_table_name"
+        self.user_selection_ddb_table_name = "user_selection_ddb_table_name"
+
+        os.environ["MOVIES_SIMILARITY_DYNAMO_DB_NAME"] = self.movie_ddb_table_name
+        os.environ["USER_SELECTION_DYNAMO_DB_NAME"] = self.user_selection_ddb_table_name
+
+        movies_dynamodb = resource("dynamodb", region_name="us-east-1")
+        user_selection_dynamodb = resource("dynamodb", region_name="us-east-1")
+
+        movies_dynamodb.create_table(
+            TableName=self.movie_ddb_table_name,
+            KeySchema=[{"AttributeName": "movieId", "KeyType": "HASH"}],
+            AttributeDefinitions=[
+                {"AttributeName": "movieId", "AttributeType": "N"},
+                {"AttributeName": "index", "AttributeType": "N"},
+            ],
+            BillingMode="PAY_PER_REQUEST",
+            GlobalSecondaryIndexes=[
+                {
+                    "IndexName": "getIndex",
+                    "KeySchema": [{"AttributeName": "index", "KeyType": "HASH"}],
+                    "Projection": {
+                        "ProjectionType": "ALL",
+                        "NonKeyAttributes": [
+                            "string",
+                        ],
+                    },
+                    "OnDemandThroughput": {
+                        "MaxReadRequestUnits": 123,
+                        "MaxWriteRequestUnits": 123,
+                    },
+                },
+            ],
+        )
+
+        user_selection_dynamodb.create_table(
+            TableName=self.user_selection_ddb_table_name,
+            KeySchema=[{"AttributeName": "email", "KeyType": "HASH"}],
+            AttributeDefinitions=[{"AttributeName": "email", "AttributeType": "S"}],
+            BillingMode="PAY_PER_REQUEST",
+        )
+
+        mocked_movies_dynamodb_resource = {
+            "resource": resource("dynamodb"),
+            "table_name": self.movie_ddb_table_name,
+        }
+
+        mocked_user_selection_dynamodb_resource = {
+            "resource": resource("dynamodb"),
+            "table_name": self.user_selection_ddb_table_name,
+        }
+
+        self.mocked_movies_dynamodb_class = LambdaDynamoDBClass(
+            mocked_movies_dynamodb_resource
+        )
+        self.mocked_user_selection_dynamodb_class = LambdaDynamoDBClass(
+            mocked_user_selection_dynamodb_resource
+        )
+
+        self.mocked_movies_dynamodb_class.table.put_item(
+            Item={"movieId": 1, "index": 1}
+        )
+
+        self.mocked_movies_dynamodb_class.table.put_item(
+            Item={"movieId": 2, "index": 2}
+        )
+
+        self.mocked_movies_dynamodb_class.table.put_item(
+            Item={"movieId": 3, "index": 3}
+        )
+
+    def test_extract_and_validate_data(self) -> None:
+        """
+        Test extract_and_validate_data function
+        """
+        from functions.setUserSelectedMovies import extract_and_validate_data
+
+        input = {
+            "email": "test@gmail.com",
+            "selectedMovies": [
+                {"movieId": "1", "similarity": ["1", "2"]},
+                {"movieId": "2", "similarity": ["1", "2"]},
+            ],
+        }
+
+        expected_return_value = "test@gmail.com", [
+            {"movieId": "1", "similarity": ["1", "2"]},
+            {"movieId": "2", "similarity": ["1", "2"]},
+        ]
+
+        result = extract_and_validate_data(input)
+        self.assertEqual(expected_return_value, result)
+
+    def load_sample_event_from_file(self, test_event_file_name: str) -> dict:
+        """
+        Loads and validate test events from the file system
+        """
+        event_file_name = f"tests/events/{test_event_file_name}.json"
+        with open(event_file_name, "r", encoding="UTF-8") as file_handle:
+            event = json.load(file_handle)
+            return event
+
+    @patch("utils.classes.LambdaDynamoDBClass")
+    def test_lambda_handler_valid_event(
+        self,
+        patch_lambda_dynamodb_class: MagicMock,
+    ):
+
+        # patch both movie_dynamodb_resource and user_dynamodb_resource
+        patch_lambda_dynamodb_class.side_effect = [
+            self.mocked_user_selection_dynamodb_class,
+            self.mocked_movies_dynamodb_class,
+        ]
+
+        expected_return_value = {
+            "statusCode": 200,
+            "headers": {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Credentials": "true",
+            },
+            "body": '{"email": "test@gmail.com", "recommendedMovies": [{"movieId": "1", "index": "1"}, {"movieId": "2", "index": "2"}], "selectedMovies": [{"movieId": "1", "index": "1"}, {"movieId": "3", "index": "3"}]}',
+        }
+
+        from functions.setUserSelectedMovies import handler
+
+        test_event = self.load_sample_event_from_file("setUserSelectedMovies")
+        actual_return_value = handler(event=test_event, context=None)
+
+        self.assertEqual(actual_return_value, expected_return_value)
+
+    def tearDown(self) -> None:
+        dynamodb_resource = client("dynamodb", region_name="us-east-1")
+        dynamodb_resource.delete_table(TableName=self.movie_ddb_table_name)
+        dynamodb_resource.delete_table(TableName=self.user_selection_ddb_table_name)
